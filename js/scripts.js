d2c5078bfdc561e0694796db18e6dd7b69d3d5b8
madddd <113197243+medthefaceless@users.noreply.github.com>
2026-04-15 19:55:43 +0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+(function () {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  for (var i = 0; i < names.length; i++) {
+    // Retrieve the first letter of the current name
+    var firstLetter = names[i].charAt(0).toLowerCase();
+
+    // Compare the 'firstLetter' to the letter 'j'
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
+  }
+})();
